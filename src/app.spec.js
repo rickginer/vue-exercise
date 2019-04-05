1a113665e0463ecc124e7e00193ab92c4a8a7169
Rick Giner <rickginer@gmail.com>
2019-04-06 10:55:37 +1100

    add (failing) test to check App is loading retailers data

---
+++ src/app.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+import AppView from '@/App.vue'
+
+describe('App.vue', () => {
+
+    it('renders to view', () => {
+        const wrapper = shallowMount(AppView, {})
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+    
+    it('loads an array of retailers data', () => {
+        const wrapper = shallowMount(AppView, {})
+        const vm = wrapper.vm
+        expect(Array.isArray(vm.retailers)).toBe(true);
+    })
+})
